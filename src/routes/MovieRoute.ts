081aad46b3206338ba6fdee29611e74805451864
Bruno <bruno.cardoso02@outlook.com>
2024-03-08 00:15:21 -0300

    getMovie Router created

--- src/routes/MovieRoute.ts
+++ src/routes/MovieRoute.ts
@@ -4,5 +4,6 @@ import MovieController from "../controllers/MovieController";
 const movieRouter = Router();
 
 movieRouter.post('/createMovie', MovieController.createMovie);
+movieRouter.get('/movies', MovieController.getMovies);
 
 export default movieRouter
