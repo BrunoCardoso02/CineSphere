85dbf2a8660a01cdc6b6114ede18fbd662b82eab
Bruno <bruno.cardoso02@outlook.com>
2024-03-08 16:45:18 -0300

    MovieRouter with deleteMovie function instead

--- src/routes/MovieRoute.ts
+++ src/routes/MovieRoute.ts
@@ -5,5 +5,6 @@ const movieRouter = Router();
 
 movieRouter.post('/createMovie', MovieController.createMovie);
 movieRouter.get('/movies', MovieController.getMovies);
+movieRouter.delete('/deleteMovie/:id', MovieController.deleteMovie);
 
 export default movieRouter
