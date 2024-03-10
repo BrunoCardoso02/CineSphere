35b678de5e6f998ca220dd2f16e22f3544d76455
Bruno <bruno.cardoso02@outlook.com>
2024-03-10 13:14:50 -0300

    Adding bcrypt and jwt on code with validations

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -8,37 +8,6 @@ interface User {
 }
 
 export default {
-  async createUser(request: Request, response: Response) {
-    try {
-      const { name, email, password } = request.body as User;
-      const userExisting = await prisma.user.findUnique({ where: { email } });
-      if (userExisting) {
-        return response.status(400).json({
-          error: true,
-          message: 'Email já cadastrado!'
-        });
-      }
-      const userCreated = await prisma.user.create({ 
-        data: {
-          name,
-          email,
-          password
-        }
-      });
-
-      return response.status(201).json({
-        message: 'Usuário Cadastrado!!!',
-        userCreated
-      });
-    } catch (err) {
-      return response.status(500).json({
-        error: true,
-        message: 'Erro ao cadastrar usuário',
-        details: err.message
-      });
-    }
-  },
-
   async getUser(request: Request, response: Response) { 
     try {
         const users = await prisma.user.findMany();
---
+++ src/controllers/auth.ts
@@ -0,0 +1,68 @@
+import { Request, Response } from 'express'
+import { prisma } from '../database';
+import {hashSync, compareSync} from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { JWT_SECRET } from '../secrets';
+
+export default {
+    async signUp (request: Request, response: Response){
+        try{
+            const {email, name, password} = request.body;
+            const user = await prisma.user.findUnique({where: {email}});
+            if(user){
+                return response.status(400).json({
+                    message: 'Esse Email já foi cadastrado'
+                });
+            };
+            const userCreated = await prisma.user.create({
+                data: {
+                    name,
+                    email,
+                    password: hashSync(password, 10)
+                }
+            });
+            response.json({
+                userCreated
+            })
+        } catch(err) {
+            return response.status(500).json({
+                error: true,
+                message: "Não foi possível criar conta",
+                details: err.message
+            })
+        }
+    },
+    async signIn(request: Request, response: Response) {
+        try{
+            const {email, password} = request.body;
+            const user = await prisma.user.findFirst({where: {email}});
+            if(!user){
+                return response.status(400).json({
+                    error: true,
+                    message: 'Não há conta cadastrada com esse email!'
+                });
+            };
+            if(!compareSync(password, user.password)){
+                return response.status(400).json({
+                    error: true,
+                    message: "Senha Incorreta!"
+                });
+            };
+
+            const token = jwt.sign({
+                userId: user.id
+            }, JWT_SECRET)
+            
+            return response.json({
+                user,
+                token
+            })
+        } catch(err){
+            return response.status(500).json({
+                error: true,
+                message: "Ocorreu algum erro na listagem!",
+                details: err.message
+            })
+        }
+    }
+}
