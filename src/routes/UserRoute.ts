7b4c27d231ffa8e1fedf30a4e52049d8cab51fd2
Bruno <bruno.cardoso02@outlook.com>
2024-03-19 18:53:08 -0300

    signin route setting

--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -1,10 +1,12 @@
 // UserRouter.ts
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import auth from '../controllers/auth';
 
 const userRouter: Router = Router();
 
-userRouter.post('/createUser', UserController.createUser);
+userRouter.post('/createUser', auth.signUp);
+userRouter.post('/signIn', auth.signIn)
 userRouter.get('/users', UserController.getUser);
 
 export default userRouter;
