c707a9e88a55f85511e52265e4d4f99952cf6783
Bruno <bruno.cardoso02@outlook.com>
2024-03-21 21:01:23 -0300

    setting firestore configs

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,17 @@
 import Express from 'express';
 import userRouter from './routes/UserRoute';
 import movieRouter from './routes/MovieRoute';
+import firebase from './config/firebase.config';
+import {getFirestore} from 'firebase/firestore'
+
 
 const app = Express();
 const port = 3000;
+const fireStore = getFirestore(firebase)
 
 app.use(Express.json());
 app.use(userRouter);
-app.use(movieRouter)
+app.use(movieRouter);
 
 app.listen(port, () => {
     console.log(`Server running on localhost:${port}`);
